cb39693acd5c734a9516ef87ba024ba7f320f7a4
cPango <Ryan.beckett@outlook.com>
2021-07-30 11:50:30 +0100

    adding task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,22 +1,51 @@
-const AddTask = () => {
+import { useState } from 'react';
+
+const AddTask = ({ onAdd }) => {
+    const [text, setText] = useState('');
+    const [day, setDay] = useState('');
+    const [reminder, setReminder] = useState(false);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if(!text) {
+            alert('Please add a task');
+            return;
+        }
+        onAdd({ text, day, reminder });
+
+        setText('');
+        setDay('');
+        setReminder(false);
+    }
+
     return (
-        <form className='m-3 p-4 justify-content-center'>
-            <div>
-                <input className='form-control' type='text' placeholder='Add Task' />
-            </div>
-            <div>
-                <input className='form-control' type='text' placeholder='Add Day & Time' />
-            </div>
-            <div className='input-group'>
-                <div className='input-group-prepend'>
-                    <div className="input-group-text">
-                        <input type='checkbox' />
-                    </div>
-                </div>
-                <label className='mb-0 ml-2'>Set reminder</label>
+        <form className='m-3 p-4 justify-content-center' onSubmit={onSubmit}>
+            <input 
+                className='form-control my-1' 
+                type='text' 
+                placeholder='Add Task' 
+                value={text} 
+                onChange={(e) => setText(e.target.value)} 
+            />
+            <input 
+                className='form-control my-1' 
+                type='text' 
+                placeholder='Add Day & Time' 
+                value={day} 
+                onChange={(e) => setDay(e.target.value)} 
+            />
+            <div className='my-1 form-check'>
+                    <input 
+                        className='form-check-input'
+                        type='checkbox'
+                        checked={reminder}
+                        value={reminder} 
+                        onChange={(e) => setReminder(e.currentTarget.checked)} 
+                    />
+                <label className='mb-0'>Set reminder</label>
                 
             </div>
-            <input className='btn float-right' type='submit' value='Save Task' />
+            <input className='btn btn-success w-100' type='submit' value='Save Task' />
         </form>
     )
 }
